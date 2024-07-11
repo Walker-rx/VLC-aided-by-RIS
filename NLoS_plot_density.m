b37f859430c3bdeb918af1071d6dc3d4d2907a4b
Walker-rx <55276006+Walker-rx@users.noreply.github.com>
2024-07-11 21:56:20 +0800

    7.11更改

--- NLoS_plot_density.m
+++ NLoS_plot_density.m
@@ -11,7 +11,7 @@ rho_mi = 0.8;
 B = 200e6;
 q = 3;
 R_pd = 0.53;
-N = 1e-21;
+N = 1e-13;
 
 xs=3; ys=3; zs=0;
 led_polar = 0; led_azi = 0;
@@ -29,7 +29,7 @@ xr = 1;   zr = 1.5;
 total_length = 7-xr;  total_hight = 2.5-zr;
 max_k = total_hight/hm;    max_l=total_length/wm;
 
-k = 5; l = 20;
+k = 8; l = 20;
 delta_wm = round( (((100*total_length)-l*100*wm)/(l-1))*10 )/10 /100;
 delta_hm = round( (((100*total_hight)-k*100*wm)/(k-1))*10 )/10 /100;
 if delta_wm <1/100
@@ -115,173 +115,309 @@ for i = 1:length(xd_save)
 
     end
 end
+total_diff = total_diff.';
+thre = 4;
+total_diff_reshape = reshape(total_diff,1,[]);
+LoS_serve_x = [];   LoS_serve_y = [];
+NLoS_serve_x = [];  NLoS_serve_y = [];
+Both_serve_x = [];  Both_serve_y = [];
+for i = 1:length(total_diff_reshape)
+    if total_diff_reshape(i)>thre
+        LoS_serve_x(end+1) = xd_save(floor((i-1)/length(yd_save))+1);
+        LoS_serve_y(end+1) = yd_save(mod(i-1,length(xd_save))+1);
+    elseif total_diff_reshape(i)<1/thre
+        NLoS_serve_x(end+1) = xd_save(floor((i-1)/length(yd_save))+1);
+        NLoS_serve_y(end+1) = yd_save(mod(i-1,length(xd_save))+1);
+    else
+        Both_serve_x(end+1) = xd_save(floor((i-1)/length(yd_save))+1);
+        Both_serve_y(end+1) = yd_save(mod(i-1,length(xd_save))+1);
+    end
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [X, Y] = meshgrid(unique(xd_save), unique(yd_save));
-Z = reshape(p_NLoS_save,length(yd_save),length(xd_save));
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
+Z_NLoS_power = reshape(p_NLoS_save,length(yd_save),length(xd_save));
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_NLoS_power(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('NLOS功率');
+% title('NLOS功率');
 
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('NLOS功率');
-title('NLOS功率');
+% figure
+% contourf(X, Y, Z_NLoS_power, 6);
+% colorbar;
+% 
+% xlabel('X');
+% ylabel('Y');
+% title('NLOS功率');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Z_LoS_power = reshape(p_LoS_save,length(yd_save),length(xd_save));
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_LoS_power(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('LOS功率');
+% title('LOS功率');
+
+% figure
+% contourf(X, Y, Z_LoS_power, 6);
+% colorbar;
+% 
+% xlabel('X');
+% ylabel('Y');
+% title('LOS功率');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Z_total_power = reshape(p_LoS_save+p_NLoS_save,length(yd_save),length(xd_save));
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_total_power(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('功率总和');
+% title('功率总和');
+
+% figure
+% contourf(X, Y, Z_total_power, 6);
+% colorbar;
+% 
+% xlabel('X');
+% ylabel('Y');
+% title('功率总和');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Z_diff = total_diff;
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_diff(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('LOS功率/NLOS功率');
+% title('LOS功率/NLOS功率');
 
 figure
-contourf(X, Y, Z, 6);
+contourf(X, Y, Z_diff, 6);
 colorbar;
 
 xlabel('X');
 ylabel('Y');
-title('NLOS功率');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = reshape(p_LoS_save,length(yd_save),length(xd_save));
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
-
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('LOS功率');
-title('LOS功率');
-
-figure
-contourf(X, Y, Z, 6);
-colorbar;
-
-xlabel('X');
-ylabel('Y');
-title('LOS功率');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = reshape(p_LoS_save+p_NLoS_save,length(yd_save),length(xd_save));
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
-
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('功率总和');
-title('功率总和');
-
-figure
-contourf(X, Y, Z, 6);
-colorbar;
-
-xlabel('X');
-ylabel('Y');
-title('功率总和');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = total_diff;
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
-
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('LOS功率/NLOS功率');
 title('LOS功率/NLOS功率');
-
-figure
-contourf(X, Y, Z, 6);
-colorbar;
-
-xlabel('X');
-ylabel('Y');
-title('LOS功率/NLOS功率');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = 1./total_diff;
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
+Z_diff_inverse = 1./total_diff;
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_diff_inverse(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('NLOS功率/LOS功率');
+% title('NLOS功率/LOS功率');
 
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('NLOS功率/LOS功率');
-title('NLOS功率/LOS功率');
-
-figure
-contourf(X, Y, Z, 6);
-colorbar;
-
-xlabel('X');
-ylabel('Y');
-title('NLOS功率/LOS功率');
+% figure
+% contourf(X, Y, Z_diff_inverse, 6);
+% colorbar;
+% 
+% xlabel('X');
+% ylabel('Y');
+% title('NLOS功率/LOS功率');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = reshape((p_LoS_save+p_NLoS_save)./p_LoS_save,length(yd_save),length(xd_save));
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
+Z_total_diff = reshape((p_LoS_save+p_NLoS_save)./p_LoS_save,length(yd_save),length(xd_save));
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_total_diff(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('功率总和/LOS功率');
+% title('功率总和/LOS功率');
 
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('功率总和/LOS功率');
-title('功率总和/LOS功率');
-
-figure
-contourf(X, Y, Z, 6);
-colorbar;
-
-xlabel('X');
-ylabel('Y');
-title('功率总和/LOS功率');
+% figure
+% contourf(X, Y, Z_total_diff, 6);
+% colorbar;
+% 
+% xlabel('X');
+% ylabel('Y');
+% title('功率总和/LOS功率');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = reshape(rate_LoS_save,length(yd_save),length(xd_save));
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
+Z_LoS_rate = reshape(rate_LoS_save,length(yd_save),length(xd_save));
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_LoS_rate(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('最低速率');
+% title('只有LoS径时的最低速率');
 
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('最低速率');
-title('只有LoS径时的最低速率');
-
-figure
-contourf(X, Y, Z, 6);
-colorbar;
-
-xlabel('X');
-ylabel('Y');
-title('只有LoS径时的最低速率');
+% figure
+% contourf(X, Y, Z_LoS_rate, 6);
+% colorbar;
+% 
+% xlabel('X');
+% ylabel('Y');
+% title('只有LoS径时的最低速率');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = reshape(rate_total_save,length(yd_save),length(xd_save));
-figure;
-trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
-colorbar;  % 添加颜色条
+Z_total_rate = reshape(rate_total_save,length(yd_save),length(xd_save));
+% figure;
+% trisurf(delaunay(X(:), Y(:)), X(:), Y(:), Z_total_rate(:), 'FaceColor', 'interp', 'EdgeColor', 'k');
+% colorbar;  % 添加颜色条
+% 
+% % 设置坐标轴标签
+% xlabel('X');
+% ylabel('Y');
+% zlabel('最低速率');
+% title('存在NLoS径时的最低速率');
 
-% 设置坐标轴标签
-xlabel('X');
-ylabel('Y');
-zlabel('最低速率');
-title('存在NLoS径时的最低速率');
-
-figure
-contourf(X, Y, Z, 6);
-colorbar;
-
-xlabel('X');
-ylabel('Y');
-title('存在NLoS径时的最低速率');
+% figure
+% contourf(X, Y, Z_total_rate, 6);
+% colorbar;
+% 
+% xlabel('X');
+% ylabel('Y');
+% title('存在NLoS径时的最低速率');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
-eval(strcat('X = RIS_x_use_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
-eval(strcat('Z = RIS_z_use_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
-plot(X,Z,'ro', 'MarkerFaceColor', 'red', 'MarkerSize', 8)
+if ~isempty(LoS_serve_x)
+    plot(LoS_serve_x,LoS_serve_y,'ro', 'MarkerFaceColor', 'red', 'MarkerSize', 8)
+    hold on
+    plot(Both_serve_x,Both_serve_y,'go', 'MarkerFaceColor', 'green', 'MarkerSize', 8)
+    if ~isempty(NLoS_serve_x)
+        hold on
+        plot(NLoS_serve_x,NLoS_serve_y,'bo', 'MarkerFaceColor', 'blue', 'MarkerSize', 8)
+        legend('LoS Service', 'Both Services', 'NLoS Service');
+    else
+        legend('LoS Service', 'Both Services');
+    end
+else
+    if ~isempty(NLoS_serve_x)
+        plot(NLoS_serve_x,NLoS_serve_y,'bo', 'MarkerFaceColor', 'blue', 'MarkerSize', 8)
+        hold on
+        plot(Both_serve_x,Both_serve_y,'go', 'MarkerFaceColor', 'green', 'MarkerSize', 8)
+        legend('NLoS Service', 'Both Services');
+    else
+        plot(Both_serve_x,Both_serve_y,'go', 'MarkerFaceColor', 'green', 'MarkerSize', 8)
+        legend('Both Services');
+    end
+end
+
+
+xlim([0,8])
+ylim([0,8])
+xlabel('X');
+ylabel('Y');
+title("服务分布图")
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure
+eval(strcat('X_use = RIS_x_use_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
+eval(strcat('Z_use = RIS_z_use_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
+plot(X_use,Z_use,'ro', 'MarkerFaceColor', 'red', 'MarkerSize', 8)
 hold on
-eval(strcat('X = RIS_x_unuse_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
-eval(strcat('Z = RIS_z_unuse_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
-plot(X,Z,'bo', 'MarkerFaceColor', 'blue', 'MarkerSize', 8)
+eval(strcat('X_unuse = RIS_x_unuse_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
+eval(strcat('Z_unuse = RIS_z_unuse_'+string(xd_for_plot)+'_'+string(zd_for_plot),';'));
+plot(X_unuse,Z_unuse,'bo', 'MarkerFaceColor', 'blue', 'MarkerSize', 8)
 xlim([0,8])
 ylim([1,3])
 xlabel('X');
 ylabel('Z');
 title("XZ wall RIS分布图")
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure;
+globalMin = min(min(Z_total_power(:)), min(Z_LoS_power(:)));
+globalMax = max(max(Z_total_power(:)), max(Z_LoS_power(:)));
+
+subplot(2, 1, 1);
+contourf(X, Y, Z_total_power);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+xlabel('X');
+ylabel('Y');
+title('功率总和');
+
+subplot(2, 1, 2);
+contourf(X, Y, Z_LoS_power);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+xlabel('X');
+ylabel('Y');
+title('LOS功率');
+colormap(jet);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure;
+globalMin = min(min(Z_total_power(:)), min(Z_LoS_power(:)));
+globalMax = max(max(Z_total_power(:)), max(Z_LoS_power(:)));
+
+subplot(2, 1, 1);
+[C,h]=contour(X, Y, Z_total_power);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+clabel(C,h)
+xlabel('X');
+ylabel('Y');
+title('功率总和');
+
+subplot(2, 1, 2);
+[C,h]=contour(X, Y, Z_LoS_power);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+clabel(C,h)
+xlabel('X');
+ylabel('Y');
+title('LOS功率');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure;
+globalMin = min(min(Z_total_rate(:)), min(Z_LoS_rate(:)));
+globalMax = max(max(Z_total_rate(:)), max(Z_LoS_rate(:)));
+
+subplot(2, 1, 1);
+contourf(X, Y, Z_total_rate);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+xlabel('X');
+ylabel('Y');
+title('存在NLoS径时的最低速率');
+
+subplot(2, 1, 2);
+contourf(X, Y, Z_LoS_rate);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+xlabel('X');
+ylabel('Y');
+title('只有LoS径时的最低速率');
+colormap(jet);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure;
+globalMin = min(min(Z_total_rate(:)), min(Z_LoS_rate(:)));
+globalMax = max(max(Z_total_rate(:)), max(Z_LoS_rate(:)));
+
+subplot(2, 1, 1);
+[C,h]=contour(X, Y, Z_total_rate);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+clabel(C,h)
+xlabel('X');
+ylabel('Y');
+title('存在NLoS径时的最低速率');
+
+subplot(2, 1, 2);
+[C,h]=contour(X, Y, Z_LoS_rate);
+caxis([globalMin globalMax]);
+colorbar('location', 'EastOutside');
+clabel(C,h)
+xlabel('X');
+ylabel('Y');
+title('只有LoS径时的最低速率');
+
 
 
 
--- test.m
+++ test.m
@@ -1,18 +1,15 @@
-clear 
-close all
+% clear 
+% close all
 
-% 创建网格化数据
-[X, Y] = meshgrid(1:5, 1:5);
-Z = X .^ 2 + Y .^ 2;
 
-% 绘制等高线图
+thresholds = [0 0.25 4];
 figure;
-contourf(X, Y, Z,ShowText="on");
 
-% 添加数值标注
-clabel(Z);
+custommap = [
+    0 0 1
+    0 1 0
+    1 0 0];
+[C, ~] = contourf(X, Y, Z_diff, thresholds);
+colormap(custommap)
 
-% 自定义图形
-title('Contour Plot with Labels');
-xlabel('X-axis');
-ylabel('Y-axis');
+clabel(C)
